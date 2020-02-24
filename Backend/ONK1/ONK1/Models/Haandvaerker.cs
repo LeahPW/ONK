9af6a30b325e9f38bb9636cbb98c593cb2700a49
Leah <valeriapwellejus@outlook.com>
2020-02-24 11:46:13 +0100

    First push

---
+++ Backend/ONK1/ONK1/Models/Haandvaerker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ONK1.Models
+{
+    public class Haandvaerker
+    {
+        public DateTime HVAnsaettelsesdato { get; set; }
+        public string HVEfternavn { get; set; }
+        public string HVFagomraade { get; set; }
+        public string HVFornavn { get; set; }
+        [Key]
+        public int HaandvaerkerId { get; set; }
+
+        public List<Vaerktoejskasse> Vaerktoejskasser { get; set; }
+    }
+}
---
+++ Backend/ONK1/ONK1/Models/Vaerktoejskasse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ONK1.Models
+{
+    public class Vaerktoejskasse
+    {
+        public Haandvaerker Haandvaerker { get; set; }
+        public DateTime VTKAnskaffet { get; set; }
+
+        [ForeignKey("Haandvaerker")]
+        public int VTKEjesAf {get;set;}
+        public string VTKfabrikat { get; set; }
+        public string VTKFarve { get; set; }
+        [Key]
+        public int VTKId { get; set; }
+        public string VTKModel { get; set; }
+        public string VTKSerienummer { get; set; }    
+
+        public List<Vaerktoej> Vaerktoejer { get; set; }
+    }
+}
