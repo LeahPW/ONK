9bb23fc8894f65e60e3fdcc7fd40b21bf99d347f
simoher <simoher@hotmail.com>
2020-03-10 18:56:31 +0100

    Revert "Comment out old controller"
    This reverts commit 99207e1ca9f8f8ac2078c45c3b60f02c9eaf3a52.

--- Backend/ONK1/Frontend/Controllers/HaandvaerkersController.cs
+++ Backend/ONK1/Frontend/Controllers/HaandvaerkersController.cs
@@ -92,81 +92,81 @@ namespace Frontend.Controllers
             return View(haandvaerker);
         }
 
-        //// GET: Haandvaerkers/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // GET: Haandvaerkers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //    var haandvaerker = await _context.Haandvaerker.FindAsync(id);
-        //    if (haandvaerker == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(haandvaerker);
-        //}
+            var haandvaerker = await _context.Haandvaerker.FindAsync(id);
+            if (haandvaerker == null)
+            {
+                return NotFound();
+            }
+            return View(haandvaerker);
+        }
 
         // POST: Haandvaerkers/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("HVAnsaettelsesdato,HVEfternavn,HVFagomraade,HVFornavn,HaandvaerkerId")] Haandvaerker haandvaerker)
-        //{
-        //    if (id != haandvaerker.HaandvaerkerId)
-        //    {
-        //        return NotFound();
-        //    }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("HVAnsaettelsesdato,HVEfternavn,HVFagomraade,HVFornavn,HaandvaerkerId")] Haandvaerker haandvaerker)
+        {
+            //if (id != haandvaerker.HaandvaerkerId)
+            //{
+            //    return NotFound();
+            //}
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(haandvaerker);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!HaandvaerkerExists(haandvaerker.HaandvaerkerId))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(haandvaerker);
-        //}
+            //if (ModelState.IsValid)
+            //{
+            //    try
+            //    {
+            //        _context.Update(haandvaerker);
+            //        await _context.SaveChangesAsync();
+            //    }
+            //    catch (DbUpdateConcurrencyException)
+            //    {
+            //        if (!HaandvaerkerExists(haandvaerker.HaandvaerkerId))
+            //        {
+            //            return NotFound();
+            //        }
+            //        else
+            //        {
+            //            throw;
+            //        }
+            //    }
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //return View(haandvaerker);
+        }
 
         // GET: Haandvaerkers/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    try
-        //    {
-        //        var client = _clientFactory.CreateClient("api");
-        //        var request = new HttpRequestMessage(HttpMethod.Delete, $"Haandvaerker/{id}");
-        //        var response = await client.SendAsync(request);
+        public async Task<IActionResult> Delete(int? id)
+        {
+            try
+            {
+                var client = _clientFactory.CreateClient("api");
+                var request = new HttpRequestMessage(HttpMethod.Delete, $"Haandvaerker/{id}");
+                var response = await client.SendAsync(request);
 
-        //        if (!response.IsSuccessStatusCode)
-        //        {
-        //            return StatusCode(StatusCodes.Status500InternalServerError, response);
-        //        }
+                if (!response.IsSuccessStatusCode)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, response);
+                }
 
-        //        var test = await response.Content.ReadAsStringAsync();
-        //        Console.WriteLine(test);
+                var test = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(test);
 
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
 
     }
 }
